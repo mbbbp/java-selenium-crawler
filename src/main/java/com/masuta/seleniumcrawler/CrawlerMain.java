467ef88aa50a796117b62b3f79588b00f5251d74
mbbbp <mbbbp@nate.com>
2021-02-24 12:56:36 +0900

    2차 커밋

--- src/main/java/com/masuta/seleniumcrawler/CrawlerMain.java
+++ src/main/java/com/masuta/seleniumcrawler/CrawlerMain.java
@@ -20,13 +20,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import com.google.gson.Gson;
 
 public class CrawlerMain {
-	
+
 	static String targetURL = "http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf";
-	
-	
+
+
 	public static void main(String[] args) throws InterruptedException {
 		  // 현재 package의 workspace 경로, Windows는 [ chromedriver.exe ]
-        Path path = Paths.get("C:/Chromedriver/chromedriver.exe");  // 현재 package의
+        Path path = Paths.get("E:/Program/ChromeDriver/chromedriver.exe");  // 현재 package의
         
         // WebDriver 경로 설정
         System.setProperty("webdriver.chrome.driver", path.toString());
@@ -43,53 +43,45 @@ public class CrawlerMain {
         
         // WebDriver 객체 생성
         ChromeDriver driver = new ChromeDriver( options );                
+        ChromeDriver driverDetail = new ChromeDriver( options );                
         
         Map<String, String> searchOptions = new HashMap<String, String>();
                 
         searchOptions.put("range", "1"); 
         searchOptions.put("kind", "0");
-        
-        
+                
         driver.get(targetURL);
         
         JavascriptExecutor js = (JavascriptExecutor) driver;
+        JavascriptExecutor jsDetail = (JavascriptExecutor) driverDetail;
         
         List<String> categoryLinkList = new ArrayList<String>();
-        // 웹페이지에서 카테고리 영역 가져오기
+        
+		// 웹페이지에서 카테고리 영역 가져오기
         WebElement categoryList = driver.findElement(By.className("list_sub_category"));
         List<WebElement> categoryItems = categoryList.findElements(By.tagName("li"));        
                 
         for (WebElement category : categoryItems) {
         	WebElement categoryLink = category.findElement(By.tagName("a"));
-   	    	categoryLinkList.add(categoryLink.getAttribute("href").replace("javascript:", ""));        	
-        	//categoryLinkList.add(getCategoryLinkSource(categoryLink.getAttribute("href")));        	
+   	    	categoryLinkList.add(categoryLink.getAttribute("href").replace("javascript:", ""));        
 		}
         
-//        for (int index=0; index < categoryLinkList.size(); index++) {
-//        	if(index > 0)
-//        		js.executeScript(categoryLinkList.get(index));
-        	//js.executeScript(categoryLinkList.get(index));
-        	
+        for (int index=0; index < categoryLinkList.size(); index++) {
+        	if(index > 0) {
+				js.executeScript(categoryLinkList.get(index));
+			}
+
         	List<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
-        	getPageDataSource(driver, js, itemList);
-        	//System.out.println(productTitle.getText());
-        	//js.executeScript("_go_targetPage('10')");
-        	
-        	System.out.println(new Gson().toJson(itemList));
-        	
-//        }
-        
-        
-       // 2. 카테고리별 페이지 리스트 가져오기
+        	getPageDataSource(driver, driverDetail, js, jsDetail,  itemList);       	        	        	        
+        }
         
         // 탭 종료
         driver.close();
+        driverDetail.close();
         Long endTime =  System.currentTimeMillis();
         
         System.out.println((endTime - startTime) / 1000 + "초 소요");                      
         
-        
-        // 5초 후에 WebDriver 종료
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -99,7 +91,7 @@ public class CrawlerMain {
             driver.quit();
         }
 	}
-	
+
 	public List<HashMap<String, Object>> getPageInfo(String URL, ChromeOptions options, ChromeDriver driver){
 		// WebDriver 옵션 설정
         
@@ -118,11 +110,12 @@ public class CrawlerMain {
         driver.close();
 		return null;
 	}
-	private static void getPageDataSource(ChromeDriver driver, JavascriptExecutor js, List<HashMap<String, Object>> itemList) {
-		
+	private static void getPageDataSource(ChromeDriver driver, ChromeDriver driverDetail, JavascriptExecutor js, JavascriptExecutor jsDetail, List<HashMap<String, Object>> itemList) {
+
 		WebElement productList = driver.findElement(By.className("list_type01"));
-    	List<WebElement> productItems = productList.findElements(By.tagName("li"));
-    	
+		List<WebElement> productItems = productList.findElements(By.tagName("li"));
+
+
 		for(WebElement productItem : productItems) {
     		try {
     			HashMap<String, Object> item = new HashMap<String, Object>();
@@ -130,11 +123,12 @@ public class CrawlerMain {
     			String additionalAuthorString = "";
     			WebElement productDetail = productItem.findElement(By.className("detail"));
         		WebElement productTitle = productDetail.findElement(By.className("title"));
+				WebElement productTitleLink = productTitle.findElement(By.tagName("a"));
         		WebElement productAuthor = productDetail.findElement(By.className("author"));
         		WebElement productPrice = productDetail.findElement(By.className("price")).findElement(By.className("book_price"));
-        		
+        
         		authorString = productAuthor.getText();
-        		
+        
         		if(authorString.contains("저자 더보기")) {
         			WebElement productAdditionalAuthor1 = productAuthor.findElement(By.className("uxOpenList"));
         			WebElement productAdditionalAuthor2 = productAdditionalAuthor1.findElement(By.className("list_author"));
@@ -146,44 +140,39 @@ public class CrawlerMain {
         					additionalAuthorString += " " + additionalAuthor.getAttribute("textContent");
         				}
 					}
-        			authorString = authorString.replace("저자 더보기", additionalAuthorString).replace("\n", "");            			
+        			authorString = authorString.replace("저자 더보기", additionalAuthorString).replace("\n", "");            
         		}
-        		
-        		item.put("title", productTitle.getText());
+        
+        
+
+				// 상세화면에서 필요한 데이터 획득
+				driverDetail.get(productTitleLink.getAttribute("href"));
+
+				WebElement isbnDetail = driverDetail.findElements(By.className("table_simple2")).get(0);
+				WebElement isbn = isbnDetail.findElements(By.tagName("span")).get(0);
+
+				item.put("title", productTitle.getText());
         		item.put("author", productTitle.getText());
         		item.put("price", productPrice.getText());
-        		
+				item.put("isbn", isbn.getText());
+        
         		itemList.add(item);
-        		
+        
 			} catch (Exception e) {
-//				e.printStackTrace();
-			}   		    		
-    		   		
+				//e.printStackTrace();
+			}   		    
+    		   
     	}
-		
 
 		WebElement pageList = driver.findElement(By.className("list_paging"));
-		
+
 		try {
 			WebElement nextButton = pageList.findElement(By.className("btn_next"));
-			//nextButton.getAttribute("href").replace("javascript:", "");
 			js.executeScript(nextButton.getAttribute("href").replace("javascript:", ""));
-			getPageDataSource(driver, js, itemList);
+			getPageDataSource(driver, driverDetail, js, jsDetail,  itemList);
 		} catch (Exception e) {
-			
+
 		}
 	}
-	
-	// 카테고리 링크 추출
-	private static String getCategoryLinkSource(String href) {	
-		href = href.replace("javascript:goCateList(", "").replace(")", "").replace("'", "").replace(" ", "");		
-				
-		String[] linkSource = href.split(",");		
-		
-		String url = targetURL + "?range=1&kind=0" + "&mallGb=" + linkSource[0] + "&linkClass=" + linkSource[1] + "&orderClick=" + linkSource[2] ;		
-				
-		return url;
-		
-	}
-	
+
 }
